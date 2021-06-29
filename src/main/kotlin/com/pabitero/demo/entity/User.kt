c4a3635ea7acc31e112d3da3ba6c11a4bf71160e
William <pabiterowilliam@yahoo.com>
2021-06-29 16:36:43 -0400

    Added database config

--- src/main/kotlin/com/pabitero/demo/entity/User.kt
+++ src/main/kotlin/com/pabitero/demo/entity/User.kt
@@ -1,6 +1,16 @@
 package com.pabitero.demo.entity
 
-data class User(val id: Int,
-                var firstName: String,
-                var lastName: String,
-                var email: String)
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
+data class User(
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long = 0,
+        @Column(name = "first_name", nullable = false)
+        var firstName: String = "",
+        @Column(name = "last_name", nullable = false)
+        var lastName: String = "",
+        @Column(name = "email", nullable = false)
+        var email: String = ""
+)
