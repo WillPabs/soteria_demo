f811958c8d5b22c49d49dc2e28bdec71341e8002
William <pabiterowilliam@yahoo.com>
2021-06-29 16:33:27 -0400

    Updated nuxtlink to proper params

--- src/main/frontend/pages/index.vue
+++ src/main/frontend/pages/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <p v-if="$fetchState.pending">Fetching Users</p>
-  <p v-else-if="$fetchState.error">Error fetching users</p>
-  <div v-else>
+  <div id="list8">
     <h1>Users</h1>
-    <ul v-for="user of users" :key="user.id">
-      <NuxtLink :to="`${user._slug}`">
-        <li>{{ user.firstName }}</li>
-      </NuxtLink>
-    </ul>
-    <ol>
-      <li v-for="user of users">
-        {{ user.firstName }} {{ user.lastName }}
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <NuxtLink
+          :to="{ name: 'users-slug', params: { id: user.id } }"
+        >
+          {{ user.firstName }}
+        </NuxtLink>
       </li>
-    </ol>
-    <button @click="$fetch">Refresh</button>
+    </ul>
   </div>
 </template>
 
@@ -24,15 +20,19 @@ export default {
       title: "User List"
     }
   },
-  data() {
-    return {
-      users: []
-    }
-  },
-  async fetch() {
-    this.users = await fetch(
-      'http://localhost:8888/api/users'
-    ).then((res) => res.json())
+  async asyncData({ $http }) {
+    const users = await $http.$get(`http://localhost:8888/api/users`)
+    console.log(users)
+    return { users }
   }
 }
 </script>
+<style>
+  /*!* LIST #8 *!*/
+  #list8 ul li { font-family:Georgia,serif,Times; font-size:18px;margin:auto; }
+  #list8 ul li a { display:block; width:300px; height:28px; background-color:#333; border-left:5px solid #222; border-right:5px solid #222; padding-left:10px;
+    text-decoration:none; color:#bfe1f1; }
+  #list8 ul li a:hover {  -moz-transform:rotate(-2deg); -moz-box-shadow:10px 10px 20px #000000;
+    -webkit-transform:rotate(-5deg); -webkit-box-shadow:10px 10px 20px #000000;
+    transform:rotate(-5deg); box-shadow:10px 10px 20px #000000; }
+</style>
