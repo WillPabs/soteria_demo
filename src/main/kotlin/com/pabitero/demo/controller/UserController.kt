57a3af0b74504f89da0b00e751e060b7ec01f69c
William <pabiterowilliam@yahoo.com>
2021-06-29 16:27:13 -0400

    Added fix to CORS policy

--- src/main/kotlin/com/pabitero/demo/controller/UserController.kt
+++ src/main/kotlin/com/pabitero/demo/controller/UserController.kt
@@ -1,19 +1,42 @@
 package com.pabitero.demo.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import com.pabitero.demo.entity.User
+import com.pabitero.demo.service.UserService
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import java.util.*
+import javax.servlet.Filter
 
-@RestController("api")
-class UserController {
+@RestController
+class UserController (private val userService: UserService) {
 
-    @GetMapping("/users")
-    fun getUsers() {
+    @GetMapping("/api/users")
+    fun getUsers(): List<User> = userService.getAllUsers()
 
+    @GetMapping("/api/user/{id}")
+    fun getUserById(@PathVariable id: Long): User = userService.getUserById(id)
+
+    @PostMapping("/api/users")
+    fun createUser(@RequestBody payload: User): User = userService.createUser(payload)
+
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        // *** URL below needs to match the Vue client URL and port ***
+        config.allowedOrigins = Collections.singletonList("http://localhost:3000")
+        config.allowedMethods = Collections.singletonList("*")
+        config.allowedHeaders = Collections.singletonList("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean<Filter>(CorsFilter(source))
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE)
+        return bean
     }
 
-    @GetMapping("/user/{id}")
-    fun getUserById(@PathVariable id: Int) {
-
-    }
 }
