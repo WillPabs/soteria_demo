b94334584d89555b792f23e9f748dbad749ed1df
William <pabiterowilliam@yahoo.com>
2021-06-28 22:06:24 -0400

    Added a schema and data files to prepopulate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (first_name, last_name, email) VALUES
+  ('William', 'Pabitero', 'willpabs@yahoo.com'),
+  ('Elon', 'Musk', 'elonmusk@tesla.com'),
+  ('Jeff', 'Bezos', 'jeffbezos@amazon.com'),
+  ('Bill', 'Gates', 'billgates@microsoft.com'),
+  ('Satoshi', 'Nakomoto', 'satoshi@bitcoin.com');
